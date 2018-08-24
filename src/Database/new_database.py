616af28bff81b5cd36b11f023dd3cd71ba98fdd8
diegothuran <diego.thuran@gmail.com>
2018-08-24 14:06:29 -0300

    adding bcc

--- src/Database/new_database.py
+++ src/Database/new_database.py
@@ -1,11 +1,11 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
-from Model import News
-from Database import connection
+from src.Model import News
+from src.Database import connection
 import datetime
 from dateutil import parser
-import postagem.Util as Util
+import src.postagem.Util as Util
 
 
 def save_news(news=News):
