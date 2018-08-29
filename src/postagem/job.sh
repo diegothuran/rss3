deaff69cadc826f24c6889b75d6c76702f98cfb6
diegothuran <diego.thuran@gmail.com>
2018-08-29 12:01:26 -0300

    adding sh file

--- src/postagem/job.sh
+++ src/postagem/job.sh
@@ -1,1 +1,9 @@
-python Rss_test.py
+#!/usr/bin/env bash
+a = 1;
+until [$_done]; do
+    python Rss_test.py;
+    python post_from_bbc.py;
+    python soup_elpais.py;
+    python soup_globo.py;
+    sleep 1;
+done
