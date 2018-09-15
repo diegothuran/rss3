50245536600c78e6b9efbad7304974118a8380d7
diegothuran <diego.thuran@gmail.com>
2018-09-15 18:20:47 -0300

    sh for midias

---
+++ src/midia_postagem/job.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+until 1; do
+    python Rss_midia.py;
+    python soup_agenciabrasil.py;
+    python soup_diplomatique.py;
+    python soup_jornaldocomercio.py;
+    python soup_operamundi.py
+    sleep 1;
+done
