825427f2ec0741c8df522ee4d6f58717854c310c
diegothuran <diego.thuran@gmail.com>
2018-10-03 17:21:19 -0300

    changing sh midias

--- src/midia_postagem/midias.sh
+++ src/midia_postagem/midias.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 until 1; do
-    python Rss_midia.py;
-    python soup_agenciabrasil.py;
-    python soup_diplomatique.py;
-    python soup_jornaldocomercio.py;
-    #python soup_operamundi.py
+    for f in *.py; do python "$f"; done
     sleep 1;
 done
