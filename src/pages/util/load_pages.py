f915bc95bb1c8be2066e7578e4c3949d3d3dce0b
victorlorena <victor.lorena@gmail.com>
2018-11-28 17:15:05 -0300

    ajeitando o load pages

--- src/pages/util/load_pages.py
+++ src/pages/util/load_pages.py
@@ -77,37 +77,37 @@ jovempan, justificando_artigos, justificando, marceloauler, ne10, oantagonista, 
 
 """ List of pages """
 PAGES = [# page from the states
-#          agazetadoacre, jornalatribuna, jornalopiniao, jornalopiniao_economia, jornalopiniao_policia, jornalopiniao_politica, oaltoacre,
-#          oestadoacre, oriobranco_geral, oriobranco_policial, oriobranco_politica, pagina20, pagina20_economia, pagina20_policia, pagina20_politica,
-#          anoticia, cadaminuto, correiodopovo, gazetadealagoas, gazetaweb, novoextra, primeiraedicao, tribunahoje, tribunauniao,
-#          aquiamapa, diariodoamapa, acritica_am, ojornaldailha, osolimoes, aregiao, atarde, correiodooeste, folharegionalbahia, folhasertaneja, 
-#          istoenoticia, jornalalerta, jornalanossavoz, jornalfolhadoestado, jornalimpacto, jornalnovafronteira, novoeste, oecojornal, rss_diariobahia, 
-#          tribunafeirense, anoticiadoceara, oestadoce, opovo, tribunadoceara, camara, correiobraziliense, folhacentrooeste, jornaldebrasilia, jornalregional, 
-#          rss_estacaonews, rss_senado, aquinoticias, correiodoestadoonline, estadocapixaba, gazetaonline, jornalcorreiocapixaba, noticiaagora, 
-#          rss_folhaonline, tribunaonline, diariodeaparecida, diariodoestadogo, jornalaguaslindas, jornalopcao, jornalestadodegoias, oanapolis, ohoje, 
-#          opopular, rss_jornalopcao, tribunadoplanalto, atosefatos, oestadoma, oquartopoder, 
-#          # pages ate o maranhao
+        agazetadoacre, jornalatribuna, jornalopiniao, jornalopiniao_economia, jornalopiniao_policia, jornalopiniao_politica, oaltoacre,
+        oestadoacre, oriobranco_geral, oriobranco_policial, oriobranco_politica, pagina20, pagina20_economia, pagina20_policia, pagina20_politica,
+        anoticia, cadaminuto, correiodopovo, gazetadealagoas, gazetaweb, novoextra, primeiraedicao, tribunahoje, tribunauniao,
+        aquiamapa, diariodoamapa, acritica_am, ojornaldailha, osolimoes, aregiao, atarde, correiodooeste, folharegionalbahia, folhasertaneja, 
+        istoenoticia, jornalalerta, jornalanossavoz, jornalfolhadoestado, jornalimpacto, jornalnovafronteira, novoeste, oecojornal, rss_diariobahia, 
+        tribunafeirense, anoticiadoceara, oestadoce, opovo, tribunadoceara, camara, correiobraziliense, folhacentrooeste, jornaldebrasilia, jornalregional, 
+        rss_estacaonews, rss_senado, aquinoticias, correiodoestadoonline, estadocapixaba, gazetaonline, jornalcorreiocapixaba, noticiaagora, 
+        rss_folhaonline, tribunaonline, diariodeaparecida, diariodoestadogo, jornalaguaslindas, jornalopcao, jornalestadodegoias, oanapolis, ohoje, 
+        opopular, rss_jornalopcao, tribunadoplanalto, atosefatos, oestadoma, oquartopoder, 
+        # pages ate o maranhao
          circuitomt, copopular, folhadoestado, gazetadigital, acritica, atribunanews, correiodoestado, jd1noticias, midiamax,
          em, folhamg, hojeemdia, correiodaparaiba, jornaldaparaiba, bemparana, diarioinduscom, impactopr, jornaldoonibusdecuritiba, tribunapr,
          correiodepernambuco, diariodepernambuco, folhape, jconline, destakjornal, jb, monitordigital, odia, oglobo, 
          agorarn, tribunadenoticias, tribunadonorte, correiodopovo, osul, correiodenoticia, diariodaamazonia, folhabv, jornalroraimahoje,
          anoticia_clicrbs, ndonline, nsctotal_santa, nsctotal, brasildefato, dci, diariodenoticias, estadao, fsp, gazetasp, horadopovo, jornalestacao, 
-         meioemensagem, metronews, valor, cinform, jornaldesergipe, jornaldodiase, agora_to, conexaoto, jornaldotocantins, ogirassol, portalstylo
+         meioemensagem, metronews, valor, cinform, jornaldesergipe, jornaldodiase, agora_to, conexaoto, jornaldotocantins, ogirassol, portalstylo,
          
-#          #paginas diversas
-#          agenciabrasil_ebc, bastidoresdopoder, bbc, cartacapital, correio24horas, diariocatarinense, diariodocentrodomundo, diariodocentrodomundo_mundo, 
-#          diariodonordeste, dinheirorural, elpais, exame, gauchazh, gazetadopovo, globo_eleicoes, infomoney, istoedinheiro, jornalestadodeminas, jovempan, 
-#          justificando_artigos, justificando, marceloauler, ne10, oantagonista, r7, rss_globo, terra_noticias, tnh1, tribunadosertao, veja_ultimas,
-# 
-#          # paginas gabriel
-#          abert, abi, administradores, adrenaline, agenciabrasil, ancine, apublica, avozeavezdajuventude, b9, balaiodokotscho, 
-#         blastingnews, blogdafloresta, blogdoataide, blogdoluciosorge, blogdomaringoni, blogdomello, blogdomiro, blogdoneylopes, blogdopaulinho, 
-#         blogdopaulonunes, blogdoprimo, blogdoriella, blogdorovai, blogdoskarlack, blogmarcosfrahm, brasildefato, buzzfeed_news_br, camara, 
-#         carta_maior, cartamaior, ceticismopolitico, cinegnose, comunicadores, congressoemfoco, convergenciadigital, correiodobrasil, diariodocentrodomundo, 
-#         diplomatique, domingoscosta, eb, ebc, elielbezerra, esporteemidia, estadao, extra_globo, fabiocampana, falandoverdades, folha_piaui, folha_sp, 
-#         gospelprime, grandesnomesdapropaganda, huffpostbrasil, imprensaviva, istoe, jaderbarbalho, jornaisvirtuais, jornaldocomercio, jornallivre, jota, 
-#         justificando, lulacerda, marcossilverio, migalhas, moneytimes, mundodomarketing, natelinha, nocaute, noticiasdatv, oab, observatoriodatelevisao, 
-#         ocafezinho, ocombatente, operamundi, osamigosdopresidentelula, osdivergentes, outraspalavras, papelpop, politicanarede, 
-#         poncheverde, portaldapropaganda, ptnacamara, revistaforum, revistapress, rufandobombo, saibamais, sputniknews, telepadi, 
-#         telesintese, tijolaco, torcedores, tribunadainternet, tribunadajustiça, valor, veja, vermelho, viomundo, rss_multiplos
+         #paginas diversas
+         agenciabrasil_ebc, bastidoresdopoder, bbc, cartacapital, correio24horas, diariocatarinense, diariodocentrodomundo, diariodocentrodomundo_mundo, 
+         diariodonordeste, dinheirorural, elpais, exame, gauchazh, gazetadopovo, globo_eleicoes, infomoney, istoedinheiro, jornalestadodeminas, jovempan, 
+         justificando_artigos, justificando, marceloauler, ne10, oantagonista, r7, rss_globo, terra_noticias, tnh1, tribunadosertao, veja_ultimas,
+ 
+         # paginas gabriel
+         abert, abi, administradores, adrenaline, agenciabrasil, ancine, apublica, avozeavezdajuventude, b9, balaiodokotscho, 
+        blastingnews, blogdafloresta, blogdoataide, blogdoluciosorge, blogdomaringoni, blogdomello, blogdomiro, blogdoneylopes, blogdopaulinho, 
+        blogdopaulonunes, blogdoprimo, blogdoriella, blogdorovai, blogdoskarlack, blogmarcosfrahm, brasildefato, buzzfeed_news_br, camara, 
+        carta_maior, cartamaior, ceticismopolitico, cinegnose, comunicadores, congressoemfoco, convergenciadigital, correiodobrasil, diariodocentrodomundo, 
+        diplomatique, domingoscosta, eb, ebc, elielbezerra, esporteemidia, estadao, extra_globo, fabiocampana, falandoverdades, folha_piaui, folha_sp, 
+        gospelprime, grandesnomesdapropaganda, huffpostbrasil, imprensaviva, istoe, jaderbarbalho, jornaisvirtuais, jornaldocomercio, jornallivre, jota, 
+        justificando, lulacerda, marcossilverio, migalhas, moneytimes, mundodomarketing, natelinha, nocaute, noticiasdatv, oab, observatoriodatelevisao, 
+        ocafezinho, ocombatente, operamundi, osamigosdopresidentelula, osdivergentes, outraspalavras, papelpop, politicanarede, 
+        poncheverde, portaldapropaganda, ptnacamara, revistaforum, revistapress, rufandobombo, saibamais, sputniknews, telepadi, 
+        telesintese, tijolaco, torcedores, tribunadainternet, tribunadajustiça, valor, veja, vermelho, viomundo, rss_multiplos
 ]
