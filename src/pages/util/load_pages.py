9da75dd4e999f993de98ae707b7936549ae1929d
victorlorena <victor.lorena@gmail.com>
2018-12-05 15:50:04 -0300

    separando o main

--- src/pages/util/load_pages.py
+++ src/pages/util/load_pages.py
@@ -76,6 +76,47 @@ diariodocentrodomundo_mundo, diariodonordeste, dinheirorural, elpais, exame, gau
 jovempan, justificando_artigos, justificando, marceloauler, ne10, oantagonista, r7, rss_globo, terra_noticias, tnh1, tribunadosertao, veja_ultimas, uol
 
 """ List of pages """
+PAGES_UOL = [uol]
+
+PAGES_RSS_GLOBO = [rss_globo]
+
+PAGES_RSS_GLOBO_ELEICOES = [globo_eleicoes]
+
+PAGES_RSS_MULTIPLOS = [rss_multiplos]
+
+PAGES_GABRIEL = [abert, abi, administradores, adrenaline, agenciabrasil, ancine, apublica, avozeavezdajuventude, b9, balaiodokotscho, 
+        blastingnews, blogdafloresta, blogdoataide, blogdoluciosorge, blogdomaringoni, blogdomello, blogdomiro, blogdoneylopes, blogdopaulinho, 
+        blogdopaulonunes, blogdoprimo, blogdoriella, blogdorovai, blogdoskarlack, blogmarcosfrahm, brasildefato, buzzfeed_news_br, camara, 
+        carta_maior, cartamaior, ceticismopolitico, cinegnose, comunicadores, congressoemfoco, convergenciadigital, correiodobrasil, diariodocentrodomundo, 
+        diplomatique, domingoscosta, eb, ebc, elielbezerra, esporteemidia, estadao, extra_globo, fabiocampana, falandoverdades, folha_piaui, folha_sp, 
+        gospelprime, grandesnomesdapropaganda, huffpostbrasil, imprensaviva, istoe, jaderbarbalho, jornaisvirtuais, jornaldocomercio, jornallivre, jota, 
+        justificando, lulacerda, marcossilverio, migalhas, moneytimes, mundodomarketing, natelinha, nocaute, noticiasdatv, oab, observatoriodatelevisao, 
+        ocafezinho, ocombatente, operamundi, osamigosdopresidentelula, osdivergentes, outraspalavras, papelpop, politicanarede, 
+        poncheverde, portaldapropaganda, ptnacamara, revistaforum, revistapress, rufandobombo, saibamais, sputniknews, telepadi, 
+        telesintese, tijolaco, torcedores, tribunadainternet, tribunadajustiça, valor, veja, vermelho, viomundo]
+
+PAGES_DIVERSOS = [agenciabrasil_ebc, bastidoresdopoder, bbc, cartacapital, correio24horas, diariocatarinense, diariodocentrodomundo, diariodocentrodomundo_mundo, 
+        diariodonordeste, dinheirorural, elpais, exame, gauchazh, gazetadopovo, infomoney, istoedinheiro, jornalestadodeminas, jovempan, 
+        justificando_artigos, justificando, marceloauler, ne10, oantagonista, r7, terra_noticias, tnh1, tribunadosertao, veja_ultimas]
+
+PAGES_ESTADOS_ATE_MARANHAO = [agazetadoacre, jornalatribuna, jornalopiniao, jornalopiniao_economia, jornalopiniao_policia, jornalopiniao_politica, oaltoacre,
+        oestadoacre, oriobranco_geral, oriobranco_policial, oriobranco_politica, pagina20, pagina20_economia, pagina20_policia, pagina20_politica,
+        anoticia, cadaminuto, correiodopovo, gazetadealagoas, gazetaweb, novoextra, primeiraedicao, tribunahoje, tribunauniao,
+        aquiamapa, diariodoamapa, acritica_am, ojornaldailha, osolimoes, aregiao, atarde, correiodooeste, folharegionalbahia, folhasertaneja, 
+        istoenoticia, jornalalerta, jornalanossavoz, jornalfolhadoestado, jornalimpacto, jornalnovafronteira, novoeste, oecojornal, rss_diariobahia, 
+        tribunafeirense, anoticiadoceara, oestadoce, opovo, tribunadoceara, camara, correiobraziliense, folhacentrooeste, jornaldebrasilia, jornalregional, 
+        rss_estacaonews, rss_senado, aquinoticias, correiodoestadoonline, estadocapixaba, gazetaonline, jornalcorreiocapixaba, noticiaagora, 
+        rss_folhaonline, tribunaonline, diariodeaparecida, diariodoestadogo, jornalaguaslindas, jornalopcao, jornalestadodegoias, oanapolis, ohoje, 
+        opopular, rss_jornalopcao, tribunadoplanalto, atosefatos, oestadoma, oquartopoder]
+
+PAGES_OUTROS_ESTADOS = [circuitomt, copopular, folhadoestado, gazetadigital, acritica, atribunanews, correiodoestado, jd1noticias, midiamax,
+        em, folhamg, hojeemdia, correiodaparaiba, jornaldaparaiba, bemparana, diarioinduscom, impactopr, jornaldoonibusdecuritiba, tribunapr,
+        correiodepernambuco, diariodepernambuco, folhape, jconline, destakjornal, jb, monitordigital, odia, oglobo, 
+        agorarn, tribunadenoticias, tribunadonorte, correiodopovo, osul, correiodenoticia, diariodaamazonia, folhabv, jornalroraimahoje,
+        anoticia_clicrbs, ndonline, nsctotal_santa, nsctotal, brasildefato, dci, diariodenoticias, estadao, fsp, gazetasp, horadopovo, jornalestacao, 
+        meioemensagem, metronews, valor, cinform, jornaldesergipe, jornaldodiase, agora_to, conexaoto, jornaldotocantins, ogirassol, portalstylo]
+
+
 PAGES = [# page from the states
         agazetadoacre, jornalatribuna, jornalopiniao, jornalopiniao_economia, jornalopiniao_policia, jornalopiniao_politica, oaltoacre,
         oestadoacre, oriobranco_geral, oriobranco_policial, oriobranco_politica, pagina20, pagina20_economia, pagina20_policia, pagina20_politica,
@@ -93,12 +134,12 @@ PAGES = [# page from the states
          agorarn, tribunadenoticias, tribunadonorte, correiodopovo, osul, correiodenoticia, diariodaamazonia, folhabv, jornalroraimahoje,
          anoticia_clicrbs, ndonline, nsctotal_santa, nsctotal, brasildefato, dci, diariodenoticias, estadao, fsp, gazetasp, horadopovo, jornalestacao, 
          meioemensagem, metronews, valor, cinform, jornaldesergipe, jornaldodiase, agora_to, conexaoto, jornaldotocantins, ogirassol, portalstylo,
-         
+          
          #paginas diversas
          agenciabrasil_ebc, bastidoresdopoder, bbc, cartacapital, correio24horas, diariocatarinense, diariodocentrodomundo, diariodocentrodomundo_mundo, 
          diariodonordeste, dinheirorural, elpais, exame, gauchazh, gazetadopovo, globo_eleicoes, infomoney, istoedinheiro, jornalestadodeminas, jovempan, 
          justificando_artigos, justificando, marceloauler, ne10, oantagonista, r7, rss_globo, terra_noticias, tnh1, tribunadosertao, veja_ultimas, uol,
- 
+  
          # paginas gabriel
          abert, abi, administradores, adrenaline, agenciabrasil, ancine, apublica, avozeavezdajuventude, b9, balaiodokotscho, 
         blastingnews, blogdafloresta, blogdoataide, blogdoluciosorge, blogdomaringoni, blogdomello, blogdomiro, blogdoneylopes, blogdopaulinho, 
---
+++ src/pessoas_postagem/main_pessoas_globo_eleicoes.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+import sys
+sys.path.insert(0, '../../src')
+
+from pessoas_postagem import util_pessoas
+from pages.util import load_pages
+
+
+""" post """
+while(True):
+    for page in load_pages.PAGES_RSS_GLOBO_ELEICOES:
+        try:
+            urls = page.get_urls()
+            news_from_globo = False
+            for url in urls:
+                print('\n' + url)
+                util_pessoas.news_from_link(url)
+
+        except Exception as e:
+            print(e)
+            pass
+
+
+# """ post """
+# # primeira_vez = True
+# for page in load_pages.PAGES:
+#     try:
+#         urls = page.get_urls()
+#         news_from_globo = False
+#         primeira = True
+#         for url in urls:
+#             if(primeira):
+#                 print('\n' + url)
+#                 util_pessoas.news_from_link(url)
+#             else:
+#                 primeira = False
+#                 print('--- primeira: ' + str(primeira))
+#                 break
+#  
+#     except Exception as e:
+#         print(e)
+#         pass
